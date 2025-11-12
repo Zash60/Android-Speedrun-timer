8f4c17d7bf669e86bf2d8196236b419084e1299f
Zash60 <xath06@gmail.com>
2025-11-12 20:39:22 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+// settings.gradle.kts
+
 pluginManagement {
     repositories {
         google()
@@ -5,5 +7,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+dependencyResolutionManagement {
+    // Esta configuração se aplica a todos os projetos e subprojetos (como :app)
+    repositories {
+        google()
+        mavenCentral()
+        // Adicionando jitpack aqui, que estava faltando no seu original,
+        // caso alguma dependência futura precise dele.
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
 rootProject.name = "FloatingSpeedrunTimer"
 include(":app")
