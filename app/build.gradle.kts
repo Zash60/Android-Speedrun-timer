a5741d3aa1627358c98c7e0524acd0a7a60d0b72
Zash60 <xath06@gmail.com>
2025-11-12 21:25:26 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.github.QuadFlask:colorpicker:0.0.13")
+    implementation("com.github.dhaval2404:colorpicker:2.3")
 
     // CORREÇÃO: Readicionando a dependência necessária para PreferenceManager
     implementation("androidx.preference:preference-ktx:1.2.1")
