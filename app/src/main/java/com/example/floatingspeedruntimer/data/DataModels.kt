68dbc2f300d4ac98f9b63a6befb1a883d9de95ae
Zash60 <xath06@gmail.com>
2025-11-14 18:42:04 -0300

    Update DataModels.kt

--- app/src/main/java/com/example/floatingspeedruntimer/data/DataModels.kt
+++ app/src/main/java/com/example/floatingspeedruntimer/data/DataModels.kt
@@ -1,6 +1,27 @@
 package com.example.floatingspeedruntimer.data
+
 import java.io.Serializable
+
 data class Game(var name: String, val categories: MutableList<Category> = mutableListOf())
-data class Category(var name: String, var personalBest: Long = 0, var runs: Int = 0, val splits: MutableList<Split> = mutableListOf(), val runHistory: MutableList<Run> = mutableListOf())
-data class Split(var name: String, var personalBestTime: Long = 0, var bestSegmentTime: Long = 0)
-data class Run(val finalTime: Long, val timestamp: Long, val segmentTimes: List<Long>) : Serializable
+
+data class Category(
+    var name: String,
+    var personalBest: Long = 0,
+    var runs: Int = 0,
+    val splits: MutableList<Split> = mutableListOf(),
+    val runHistory: MutableList<Run> = mutableListOf()
+)
+
+data class Split(
+    var name: String,
+    var personalBestTime: Long = 0,
+    var bestSegmentTime: Long = 0,
+    // NOVO CAMPO para armazenar o caminho absoluto da imagem do autosplit
+    var autoSplitImagePath: String? = null
+)
+
+data class Run(
+    val finalTime: Long,
+    val timestamp: Long,
+    val segmentTimes: List<Long>
+) : Serializable
